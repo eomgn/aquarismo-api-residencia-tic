dcbc7792f0c9e3d94387ad62b08a4c109633fb54
eomgn <matheeusgn21@gmail.com>
2025-09-18 01:33:24 -0300

    implementando cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { routes } from "./routes/index";
 import { errorHandling } from "./middlewares/error-handling";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // public routes
 app.use(routes);
